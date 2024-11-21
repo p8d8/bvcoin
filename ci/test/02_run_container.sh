f6d57d6ce643ecf8c602b0654aab2195159eb0a3
p8d8 <124884291+p8d8@users.noreply.github.com>
2024-11-21 17:09:48 +0300

    Update 02_run_container.sh

--- ci/test/02_run_container.sh
+++ ci/test/02_run_container.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2018-present The Bitcoin Core developers
+# Copyright (c) 2018-present The Bvcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C.UTF-8
-export CI_IMAGE_LABEL="bitcoin-ci-test"
+export CI_IMAGE_LABEL="bvcoin-ci-test"
 
 set -ex
 
@@ -59,7 +59,7 @@ if [ -z "$DANGER_RUN_CI_ON_HOST" ]; then
   fi
 
   if [ "$DANGER_CI_ON_HOST_CCACHE_FOLDER" ]; then
-   # Temporary exclusion for https://github.com/bitcoin/bitcoin/issues/31108
+   # Temporary exclusion for https://github.com/bvcoin/bvcoin/issues/31108
    # to allow CI configs and envs generated in the past to work for a bit longer.
    # Can be removed in March 2025.
    if [ "${CCACHE_DIR}" != "/tmp/ccache_dir" ]; then
